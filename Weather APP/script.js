5c2b9e3e84a8a7e4a028dde622a669199eed9edc
tugbasecil <78273998+tugbasecil@users.noreply.github.com>
2022-09-20 17:51:28 +0300

    Hava Durumu APP

---
+++ Weather APP/script.js
@@ -0,0 +1,38 @@
+const url ='https://api.openweathermap.org/data/2.5/'
+const key='5356d331112bac9ac13cdee214a97cda'
+const searchBar = $("#searchbar")
+
+const setQuery = (e) => {
+    if(e.keyCode==13)
+     getResult($(searchBar).val())
+}
+
+const getResult = (cityName) => {
+    let query= `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`
+    fetch(query)
+    .then(weather=>{
+        return weather.json()
+    })
+    
+    .then(displayResult)
+}
+
+const displayResult = (result) => {
+    let city = document.querySelector('.city')
+    city.innerText = `${result.name}, ${result.sys.country}`
+
+    let temp = document.querySelector('.temp')
+    temp.innerText = `${Math.round(result.main.temp)}°C`
+
+    let desc = document.querySelector('.desc')
+    desc.innerText = result.weather[0].description
+
+    let minmax = document.querySelector('.minmax')
+    minmax.innerText = `${Math.round(result.main.temp_min)}°C/${Math.round(result.main.temp_max)}°C`
+}
+
+const searchbar = document.getElementById("searchbar");
+searchbar.addEventListener('keypress', setQuery);
+
+
+
